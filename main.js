c67fa36c9fd59a26a1477f05d4085ce0e8dd9d8b
Nicholas Smith <npsmith1990@gmail.com>
2015-11-15 18:07:10 -0600

    set up aws api call

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+// Set up express framework
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded(
   {extended:true}
 ));
 app.use(bodyParser.json());
 
+// Set up context.io
 var ContextIO = require('contextio');
 var ctxCfg = require('./contextConfig');
 var ctxioClient = new ContextIO.Client('2.0', {
@@ -14,9 +15,17 @@ var ctxioClient = new ContextIO.Client('2.0', {
   secret: ctxCfg.secret
 });
 
+// Set up AWS
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1'
+var machinelearning = new AWS.MachineLearning();
 
 app.get('/',function (req, res) {
-  res.send('GET request to homepage');
+  ctxioClient.accounts(ctxCfg.aol).messages().get({include_body:1}, function (err, response) {
+    if (err) throw err;
+    var body = response;
+    res.status(200).send(body);
+  });
 });
 
 app.post('/', function (req, res) {
@@ -24,19 +33,41 @@ app.post('/', function (req, res) {
 });
 
 app.post('/received', function(req, res){
-  console.log(req.body.message_id);
   var message_id = req.body.message_id;
 
   // get content of message
   // https://api.context.io/2.0/accounts/id/messages/message_id
   ctxioClient.accounts(ctxCfg.unsignedn).messages(message_id).get({include_body:1}, function (err, response) {
     if (err) throw err;
+    // body[0] is text/plain
     var body = response.body.body[0].content
     var msg = "Successfully pulled content from email: " + message_id;
-    // run it through Amazon ML
 
-    // move it to folder dictated by ML using contextio
-    res.status(200).send(body);
+    // run it through Amazon ML
+    var prediction;
+    var params = {
+      MLModelId: 'ml-m1am1zq4td2',
+      PredictEndpoint: 'https://realtime.machinelearning.us-east-1.amazonaws.com', /* required */
+      Record: {
+        body: body
+      }
+    };
+    machinelearning.predict(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        // Return
+        var label = data.Prediction.predictedLabel;
+        if (label == 0) {
+          prediction = "Not an Amazon Receipt";
+        } else {
+          prediction = "Amazon Receipt";
+        }
+        res.status(200).send();
+      }
+    });
+
+    // @TODO move it to folder dictated by ML using contextio
+
   });
 
 
