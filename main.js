d114d75b8ea24f907edfbaccb065063308e1fe01
Nick P Smith <npsmith1990@gmail.com>
2015-11-27 20:47:24 -0700

    move log

--- main.js
+++ main.js
@@ -105,8 +105,8 @@ app.get('/unsignedn',function (req, res) {
 
 app.post('/received', function(req, res) {
 
-  var message_id = req.body.body.message_id;
   console.log(req.body);
+  var message_id = req.body.body.message_id;
   var errored = false;
 
 
